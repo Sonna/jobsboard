58151fda7c31389f660ccf27a3812b7c369a20d4
Lin Yang <lin.yang@rea-group.com>
2018-07-26 16:54:20 +1000

    remove app_console.rb file from git

--- app_console.rb
+++
@@ -1,8 +0,0 @@
-require 'httparty'
-require_relative 'db_config'
-require_relative 'models/job'
-require_relative 'models/user'
-require_relative 'models/favourite_job'
-require 'pry'
-
-binding.pry
