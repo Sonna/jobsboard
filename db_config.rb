aba9b8aca5728af5df78fe23ddcdcbde826b0c7e
Lin Yang <lin.yang@rea-group.com>
2018-07-27 11:00:55 +1000

    deploy to heroku

--- db_config.rb
+++ db_config.rb
@@ -5,4 +5,5 @@ options = {
   database: 'jobsboard'
 }
 
-ActiveRecord::Base.establish_connection(options)
+# ActiveRecord::Base.establish_connection(options)
+ActiveRecord::Base.establish_connection( ENV['DATABASE_URL'] || options)
--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
      
 require 'sinatra'
-require 'sinatra/reloader'
+# require 'sinatra/reloader'
 require 'sinatra/flash'
 require 'httparty'
 require_relative 'db_config'
